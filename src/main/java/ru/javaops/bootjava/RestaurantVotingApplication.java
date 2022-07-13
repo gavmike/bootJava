bd97dc217476a036ab1a4f2c68b8aec3f0bc7a68
mike <gavrilin.mike@gmail.com>
2022-07-13 17:43:10 +0300

    new Array

--- src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
+++ src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
@@ -17,10 +17,7 @@ import ru.javaops.bootjava.service.UserDetailService;
 import ru.javaops.bootjava.web.ExapleService;
 import ru.javaops.bootjava.web.UsersJob;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @SpringBootApplication
 @AllArgsConstructor
@@ -51,6 +48,7 @@ public class RestaurantVotingApplication implements CommandLineRunner  {
 
     @Override
     public void run(String... args)  {
+        new ArrayList<>();
 
         System.out.println(userDetailsRepository.findAll().size());
         System.out.println(addressRepository.findAll().size());
